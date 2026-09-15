85299ea754159a8bd92986f0bec7ccb27d0ed077
Paul-Andrei Bujor <git@paulbujor.net>
2026-09-15 20:49:09 +0200

    fix: add missing eslint-disable comment on bottom tab group

--- src/lib/components/tab-bar.svelte
+++ src/lib/components/tab-bar.svelte
@@ -237,6 +237,7 @@ fire a TMDB request on every pass of the cursor. -->
 			{@const Icon = ICONS[def.id]}
 			{@const isSelected = selected === def.id}
 			<li class="flex min-w-0 flex-1 sm:flex-none xl:w-full xl:flex-none">
+				<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
 				<a
 					href={tabs.href(def.id)}
 					data-sveltekit-noscroll
